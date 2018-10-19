fa57abd7d8b84e98ed291f4d29dcecb4b73c4c4b
Dirk Gustke <dirk.gustke@verticalmedia.com>
2018-10-19 15:57:27 +0200

    master - ignore target file

--- basic/src/BasicJob.php
+++ basic/src/BasicJob.php
@@ -9,6 +9,6 @@ class BasicJob implements Job
     public function perform(array $arguments): void
     {
         echo "foo";
-        file_put_contents(__DIR__ . '/basic_test', var_export($arguments, true) . "\n", FILE_APPEND);
+        file_put_contents(__DIR__ . '/basic_test.txt', var_export($arguments, true) . "\n", FILE_APPEND);
     }
 }
